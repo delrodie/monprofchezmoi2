2a7e48401a42fd373be67107af5d8059c2c1b1ba
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2021-08-14 05:58:40 +0000

    Change Password

---
+++ src/Controller/ChangePasswordController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangePasswordType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/change-password")
+ */
+class ChangePasswordController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_change_password", methods={"GET","POST"})
+     */
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $form = $this->createForm(ChangePasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $user = $this->getUser(); //dd($user);
+
+            $password = $passwordHasher->hashPassword(
+                $user,
+                $form->get('password')->getData()
+            );
+
+            $user->setPassword($password);
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', "Mot de passe changé avec succès");
+
+            return $this->redirectToRoute('app_logout');
+        }
+
+        return $this->render('change_password/index.html.twig', [
+            'resetForm' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            //->add('email')
+            //->add('roles')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                    'label' => 'New password',
+                    'attr' => ['class' => 'form-control']
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr' => ['class' => 'form-control']
+                ],
+                'invalid_message' => 'The password fields must match.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+            //->add('connexion')
+            //->add('lastConnectedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
